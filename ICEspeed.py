9cb9c23fccaaec5a9febe185929319d762f5e485
Ben Carp <developer@bencarp.com>
2023-08-10 13:57:10 +0200

    Simple ICE speed script via iceportal
    Selenium and headless firefox for now, would be lovely to get it without javascript, but might be impossible

---
+++ ICEspeed.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from time import sleep
+import re
+
+service = Service(executable_path='./Downloads/geckodriver')
+options = webdriver.FirefoxOptions()
+options.add_argument('-headless')
+driver = webdriver.Firefox(service=service, options=options)
+
+try:
+	while (True):
+		driver.get('https://iceportal.de')
+
+		sleep(5) # Typically long enough for javascript to run, yet not so long as to defeat the purpose
+
+		for line in driver.page_source.split('\n'):
+			if 'traininfoIntro-speedIndicatorText">' in line:
+				speed = re.search(r'speedIndicatorText">(.*)\skm/h', line).group(1)
+				print(speed + " km/h")
+
+except KeyboardInterrupt:
+	driver.quit()
